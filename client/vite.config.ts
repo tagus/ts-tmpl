a61b63bc4adc171627ea8f6f2e3c018e150b6c95
Sugat Poudel <taguspoudel@gmail.com>
2025-06-08 19:25:37 -0400

    tailwind cleanup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
@@ -13,4 +14,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
 })
